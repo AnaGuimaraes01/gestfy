9e587695b7d06daf3ff893af17314aacc65c5bf4
Ana_Carla_Guimarães <annacarlabarbosaguimaraes49@gmail.com>
2025-12-18 08:41:30 -0300

    TESTE

--- backend/src/main/java/com/empresa/gestfy/models/Cliente.java
+++ backend/src/main/java/com/empresa/gestfy/models/Cliente.java
@@ -12,8 +12,11 @@ public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
+
     private String telefone;
+    
     private String email;
 
     @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
